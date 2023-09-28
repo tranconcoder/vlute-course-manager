4850b79cbf4042f22b74df3eba431ee42a9b4f2b
tranvancon <tranvanconkg@gmail.com>
2023-09-28 19:38:34 +0700

    build formHome

---
+++ Home.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace vlute_course_manager
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BorderColor = System.Drawing.Color.ForestGreen;
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.BorderThickness = 2;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(500, 860);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // Home
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.guna2Panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
+            this.Text = "Trang chủ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,86 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Design;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vlute_course_manager
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        // Properties
+        private String EMPTY_USERNAME_ALERT = "Vui lòng điền tên đăng nhập.";
+        private String EMPTY_PASSWORD_ALERT = "Vui lòng điền mật khẩu.";
+
+        // Methods
+        private bool emptyValidateTextBox(Guna2HtmlLabel alertElm, String value, String msg)
+        {
+            if (value.Length == 0)
+            {
+                alertElm.Text = msg;
+                alertElm.Show();
+
+                return false;
+            }
+            else
+            {
+                alertElm.Hide();
+
+                return true;
+            }
+        }
+
+        // Events
+        // Username
+        private void frmUsernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            this.emptyValidateTextBox(
+                frmUsernameAlert,
+                frmUsernameTextBox.Text,
+                this.EMPTY_USERNAME_ALERT
+            );
+        }
+        // Password
+        private void frmPasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            this.emptyValidateTextBox(
+                frmPasswordAlert,
+                frmPasswordTextBox.Text,
+                this.EMPTY_PASSWORD_ALERT
+            );
+        }
+        // Login button
+        private void frmLoginButton_Click(object sender, EventArgs e)
+        {
+            // Username check
+            bool validateUsernameResult = emptyValidateTextBox(
+                frmUsernameAlert,
+                frmUsernameTextBox.Text,
+                EMPTY_USERNAME_ALERT
+            );
+            // Validate check
+            bool validatePasswordResult = emptyValidateTextBox(
+                frmPasswordAlert,
+                frmPasswordTextBox.Text,
+                EMPTY_PASSWORD_ALERT
+            );
+
+            if (validateUsernameResult && validatePasswordResult)
+            {
+                Home HomeForm = new Home();
+                HomeForm.Show();
+            }
+        }
+    }
+}
